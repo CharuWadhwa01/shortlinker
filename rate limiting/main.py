5411ab21f9a6dc889981b39d9e533cf48ee06208
Charu Wadhwa <wadhwa.charu01@gmail.com>
2025-06-30 17:59:10 +0530

    Delete rate limiting directory

--- rate limiting/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi.requests import Request
-from datetime import datetime, timedelta
-
-rate_limit_store = {}  # IP: [timestamp1, timestamp2, ...]
-RATE_LIMIT = 5
-WINDOW = timedelta(minutes=1)
